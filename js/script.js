0e03a9f9c98b42b6b30036ac07a0e3cba5ee5be8
Anessan <vova.tverdov@gmail.com>
2024-04-10 01:41:42 +0300

    NavBar

--- js/script.js
+++ js/script.js
@@ -3,9 +3,3 @@ import { Application } from '@splinetool/runtime';
 const canvas = document.getElementById('canvas3d');
 const app = new Application(canvas);
 app.load('https://prod.spline.design/DFzoNkX0hCc2VMDf/scene.splinecode');
-
-// LOADER LOGO
-
-const loader_logo = document.getElementById('loader_logo');
-const loader_logo_app = new Application(loader_logo);
-loader_logo_app.load('https://prod.spline.design/ZrimQbtzzwhn5vDM/scene.splinecode');
